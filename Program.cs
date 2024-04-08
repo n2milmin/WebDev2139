33d86d36f46a973f0f041d5ecf7cebf412d23d90
Nicole Milmine <nicoleamilmine@gmail.com>
2024-04-08 14:56:17 -0400

    ef

--- Program.cs
+++ Program.cs
@@ -5,80 +5,62 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Lab2.Services;
 using Lab2.Areas.ProjectManagement.Models;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-//builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContext>();
-
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddDefaultUI()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-
-// ensures whenever an iemailsender is injected, an instance of EmailSender is provided
-builder.Services.AddSingleton<IEmailSender, EmailSender>();
-
-var app = builder.Build();
-
-builder.Services.ConfigureApplicationCookie(options =>
+namespace Lab2
 {
-    // Cookie settings
-    options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-
-    options.LoginPath = "/Identity/Account/Login";
-    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-    options.SlidingExpiration = true;
-});
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseStatusCodePagesWithRedirects("/Home/NotFount?statusCode={0}");
-    app.UseHsts();
-}
-else
-{
-    app.UseDeveloperExceptionPage();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-app.UseAuthentication();
-
-app.MapRazorPages();
-
-
-app.MapControllerRoute(
-    name:"areas",
-    pattern:"{area:exists}/{controller=Projects}/{action=Index}/{id?}");
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-/*using(var scope = app.Services.CreateScope())
-{
-    var roleManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityRole>> ();
-    var roles = new[] { "Admin", "Member" };
-    foreach(var role in roles)
+    public class Program
     {
-        if (!await roleManager.RoleExistsAsync(role))
+        public static void Main(string[] args)
         {
-            await roleManager.CreateAsync(new IdentityRole(role));
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                            .AddEntityFrameworkStores<AppDbContext>()
+                            .AddDefaultUI()
+                            .AddDefaultTokenProviders();
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddSingleton<IEmailSender, EmailSender>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseStatusCodePagesWithRedirects("/Home/NotFount?statusCode={0}");
+                app.UseHsts();
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.MapRazorPages();
+
+
+            app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Projects}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
         }
     }
-}*/
-
-app.Run();
+}
